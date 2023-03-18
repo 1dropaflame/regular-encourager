7538ff0fa00e9d7b14f15103df739774cef94c9a
1dropaflame <1dropaflame@gmail.com>
2023-03-17 21:58:47 -0500

    - uses K8s config map to read environment variables

--- encourage/src/main/java/com/dropaflame/encourage/EncouragerController.java
+++ encourage/src/main/java/com/dropaflame/encourage/EncouragerController.java
@@ -3,6 +3,7 @@ package com.dropaflame.encourage;
 import com.dropaflame.encourage.model.Encouragement;
 import com.dropaflame.encourage.service.EncouragementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import java.util.List;
 public class EncouragerController {
     @Autowired
     EncouragementService encouragementService;
+    @Value("${GREETING_FROM_ENVIRONMENT_VARIABLE:default}")
+    String greeting;
     @GetMapping("/quote")
     public Encouragement quote(@RequestParam("mood") String mood) {
         return encouragementService.quote(mood);
@@ -24,7 +27,7 @@ public class EncouragerController {
     }
     @GetMapping("")
     public String alive() {
-        return "I am alive, yay!";
+        return greeting + " I am alive, yay!";
     }
 }
 
